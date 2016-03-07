acfe030a7ebdb71570f305097cb04a1849b1b8a2
aguestuser <aguestuser@riseup.net>
2016-03-07 22:29:44 +0100

    Swap Star Wars API for Rotten Tomatoes
    Refactor file structure
    
    * extract api to module
    * extract `App` component from index
    * consolidate `test` and `main` dirs in `src`
    * change classnames
    
    Refactor view
    * render people instead of movies (strip images, etc..)
    * style people views
    * add sample data for star wars people

--- index.android.js
+++ index.android.js
@@ -1,113 +1,17 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
 'use strict';
+
 import React, {
   AppRegistry,
   Component,
-  Image,
-  ListView,
-  StyleSheet,
-  Text,
-  View
 } from 'react-native';
 
-const reqUrl = 'https://raw.githubusercontent.com/facebook/react-native/master/docs/MoviesExample.json';
+import {App} from './src/main/containers/App';
 
-class MovieProject extends Component {
-
-  state = {
-    dataSource: new ListView.DataSource({
-      rowHasChanged: (r1, r2) => r1 !== r2
-    }),
-    loaded: false
-  };
-
-  componentDidMount(){
-    this.fetchData();
-  }
-
-  render() {
-    return this.state.loaded ?
-      this.renderMovies() :
-      this.renderLoading();
-  }
-
-renderMovie(m) {
-    return (
-      <View style={styles.container}>
-        <Image
-          source={{uri: m.posters.thumbnail}}
-          style={styles.thumbnail}
-        />
-        <View style={styles.rightContainer}>
-          <Text style={styles.title}>{m.title}</Text>
-          <Text style={styles.year}>{m.year}</Text>
-        </View>
-      </View>
-    );
-  }
-
-  renderMovies() {
-    return (
-      <ListView
-        style={styles.listView}
-        renderRow={this.renderMovie}
-        dataSource={this.state.dataSource}
-      />
-    );
-  }
-
-  renderLoading(){
-    return (
-      <View style={styles.container}>
-        <Text>
-          Loading movies...
-        </Text>
-      </View>
-    );
-  }
-
-  fetchData(){
-    fetch(reqUrl)
-      .then(res => res.json())
-      .then(data => this.setState({
-        dataSource: this.state.dataSource.cloneWithRows(data.movies),
-        loaded: true
-      }))
-      .done();
+class StarWars extends Component {
+  
+  render(){
+    return(<App/>);
   }
 }
 
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF'
-  },
-  thumbnail: {
-    width: 53,
-    height: 81
-  },
-  rightContainer: {
-    flex: 1
-  },
-  title: {
-    fontSize: 20,
-    marginBottom: 8,
-    textAlign: 'center'
-  },
-  year: {
-    textAlign: 'center'
-  },
-  listView: {
-    paddingTop: 20,
-    backgroundColor: '#F5FCFF'
-  }
-});
-
-AppRegistry.registerComponent('MovieProject', () => MovieProject);
+AppRegistry.registerComponent('StarWars', () => StarWars);
---
+++ src/main/modules/api.js
@@ -0,0 +1,12 @@
+const apiUrl = 'http://swapi.co/api';
+
+export const getMovies = () =>
+  fetch(apiUrl)
+  .then(res => res.json())
+  .then(data => data.movies);
+
+
+export const getPeople = () =>
+  fetch(`${apiUrl}/people`)
+    .then(res => res.json())
+    .then(data => data.results);
